5e7e856af0e271490904513313d9cc36085495fa
Graeme Collins <graeme.collins.0@gmail.com>
2011-02-03 03:00:23 -0600

    Created class Payment for api resource ListingPayment

---
+++ lib/getsy/payment.rb
@@ -0,0 +1,37 @@
+module Getsy
+
+  # Resource: Payment
+  #   http://developer.etsy.com/docs/resource_listingpayment
+  class Payment
+    attr_accessor(
+      :listing_payment_id,
+      :allow_check,
+      :allow_mo,
+      :allow_other,
+      :allow_paypal
+    )
+    attr_reader(
+      :country,
+      :listing
+    )
+    
+    def self.get_listing_payment(listing_payment_id, options = nil)
+      response = Getsy.api_call("/payments/#{listing_payment_id}", options)
+      new(response)
+    end
+ 
+    def self.get_listing_payment_info(listing_id, options = nil)
+      payments = []
+      response = Getsy.api_call("/listings/#{listing_id}/payments", options)
+      response.each do |payment|
+        payments.push(new(payment))
+      end
+      payments
+    end
+    
+    def initialize(params = nil)
+      Getsy.build_from_params(params) if params
+    end
+  end
+
+end
